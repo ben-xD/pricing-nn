cd793d7573f8a0894d3c0cb249b07789c1f8b47a
Davide Locatelli <dl1019@corona09.doc.ic.ac.uk>
2020-02-27 18:20:17 +0000

    splitting data

--- NeuralNet.py
+++ NeuralNet.py
@@ -6,19 +6,16 @@ class NeuralNet(nn.Module):
 
     def __init__(self, input_size, hidden_size, output_size):
         super(NeuralNet, self).__init__()
-        # self.fc1 = nn.Linear(input_size, output_size)
+        
         self.fc1 = nn.Linear(input_size, hidden_size)
-        self.norm = nn.BatchNorm1d(num_features=hidden_size)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(hidden_size, output_size)
-        # self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         out = self.fc1(x)
-        out = self.norm(out)
         out = self.relu(out)
         out = self.fc2(out)
-        # out = self.sigmoid(out)
+
         return out
 
     def accuracy(self, out, yb, batch_size):
