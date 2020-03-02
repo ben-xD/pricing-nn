328d1610c721e128e1e4b5b0132788272e84ed7d
Michael George <michael_g_2@yahoo.co.uk>
2020-03-02 11:01:22 +0000

    added new neural net and hyperparameter tuning

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,15 +1,21 @@
 from torch import nn
 
-
 class NeuralNet(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, num_layers, layer_sizes, output_size):
         super(NeuralNet, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
+
+        print(layer_sizes)
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, output_size)
+        self.linears = nn.ModuleList([nn.Linear(input_size, layer_sizes[0])])
+        self.linears.extend([nn.Linear(layer_sizes[i], layer_sizes[i+1]) for i in range(0, num_layers-1)])
+        self.linears.append(nn.Linear(layer_sizes[num_layers-1], output_size))
+        print(self.linears)
 
     def forward(self, x):
-        out = self.fc1(x)
-        out = self.relu(out)
-        out = self.fc2(out)
-        return out
+        y = x
+        for i in range(len(self.linears)):
+          y = self.linears[i](y)
+          #Apply relu to all layers but the last
+          if(i < len(self.linears) - 1):
+            y = self.relu(y)
+        return y 
